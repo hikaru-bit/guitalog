109817801bc52bba51f1dcc665250695af21952a
hikaru-bit <mracademix@gmail.com>
2021-02-10 10:25:58 +0000

    app

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '089182c48f3fc93d48874ecb199e9e74e2d21b1554c9175f4a6fba0a55dd199c5dd4cd1e4163507db750dd013478eb9cc2aa648062acd5bafe62de5d6612cbca'
