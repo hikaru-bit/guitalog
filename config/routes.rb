369f3214fb215cfe05bbb1c316472cc564c8bd73
hikaru-bit <mracademix@gmail.com>
2021-02-13 01:44:35 +0000

    app

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#top'
   get "home/about"
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :favorites, only: [:create, :destroy]
+    resources :post_comments, only: [:create, :destroy]
+  end
   
 end
